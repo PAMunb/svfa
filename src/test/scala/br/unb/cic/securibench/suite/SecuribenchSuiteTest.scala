3d20d63e3b71e77d6eaeb35cbeb48622cb011eb4
jose clavo tafur <jclavotafur@gmail.com>
2025-07-23 19:04:44 -0300

    rename to SecuribenchAllTest

--- src/test/scala/br/unb/cic/securibench/suite/SecuribenchSuiteTest.scala
+++ src/test/scala/br/unb/cic/securibench/suite/SecuribenchSuiteTest.scala
@@ -2,7 +2,7 @@ package br.unb.cic.securibench.suite
 
 import br.unb.cic.securibench.SecuribenchRuntimeTest
 
-class SecuribenchAllTest extends SecuribenchRuntimeTest {
+class SecuribenchSuiteTest extends SecuribenchRuntimeTest {
    def basePackage(): String = "securibench.micro"
 
    def entryPointMethod(): String = "doGet"
