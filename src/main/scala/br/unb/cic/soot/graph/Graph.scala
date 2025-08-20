71e66e5dbbeedd49a2fdf760f9b2d1cb4f324ecc
jose clavo tafur <jclavotafur@gmail.com>
2025-08-19 22:10:42 -0300

    Revert "[fix] Use the call graph to identify the possible targets of a method call."
    This reverts commit ae3bfa15a5ccd3e12490ec601d44dfaa5a3d8852.

--- src/main/scala/br/unb/cic/soot/graph/Graph.scala
+++ src/main/scala/br/unb/cic/soot/graph/Graph.scala
@@ -122,11 +122,7 @@ case class StringLabel(label: String) extends EdgeLabel {
   override val labelType: LabelType = SimpleLabel
 }
 
-case class ContextSensitiveRegion(
-    statement: Statement,
-    calleeMethod: String,
-    context: Set[String]
-)
+case class ContextSensitiveRegion(statement: Statement, calleeMethod: String, context: Set[String])
 
 case class CallSiteLabel(
     csRegion: ContextSensitiveRegion,
@@ -488,8 +484,8 @@ class Graph() {
     })
 
     // check if there path has only one context
-    if (!isValidContext(csOpen, csClose)) {
-      return false
+    if (! isValidContext(csOpen, csClose)) {
+        return false
     }
 
     // Get all the cs) without a (cs
@@ -515,10 +511,7 @@ class Graph() {
     return validCS
   }
 
-  def isValidContext(
-      csOpen: List[CallSiteLabel],
-      csClose: List[CallSiteLabel]
-  ): Boolean = {
+  def isValidContext(csOpen: List[CallSiteLabel], csClose: List[CallSiteLabel]): Boolean = {
     var cs: Set[String] = Set()
 
     val csOpenAndClose = csOpen ++ csClose
@@ -532,8 +525,7 @@ class Graph() {
     cs.size <= 1
   }
 
-  def nodes(): scala.collection.Set[GraphNode] =
-    graph.nodes.map(node => node.toOuter).toSet
+  def nodes(): scala.collection.Set[GraphNode] = graph.nodes.map(node => node.toOuter).toSet
 
   def edges(): scala.collection.Set[GraphEdge] = graph.edges
     .map(edge => {
@@ -626,14 +618,9 @@ class Graph() {
         "\"" + e.from.show() + "\"" + " -> " + "\"" + e.to.show() + "\""
       var l = e.label
       val label: String = e.label match {
-        case c: CallSiteLabel => {
-          if (c.labelType == CallSiteOpenLabel) {
-            s"""[label="CS([${if (c.value.context.nonEmpty)
-                c.value.context.head}]"]"""
-          } else {
-            s"""[label="CS)[${if (c.value.context.nonEmpty)
-                c.value.context.head}]"]"""
-          }
+        case c: CallSiteLabel =>  {
+          if (c.labelType == CallSiteOpenLabel) { s"""[label="CS([${ if (c.value.context.nonEmpty) c.value.context.head }]"]""" }
+          else { s"""[label="CS)[${ if (c.value.context.nonEmpty) c.value.context.head }]"]""" }
 //          if (c.labelType == CallSiteOpenLabel) { s"""[label="CS(${c.value.statement.stmt} [${c.value.context.head}]"]""" }
 //          else { s"""[label="CS)${c.value.statement.stmt} [${c.value.context.head}]"]""" }
         }
