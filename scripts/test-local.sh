92f7d11bcd98324d7a695175c4470c4cc956eb33
jose clavo tafur <jclavotafur@gmail.com>
2025-08-25 22:27:51 -0300

    removing TaintBench tests  from github actions

--- scripts/test-local.sh
+++ scripts/test-local.sh
@@ -6,8 +6,8 @@ echo "Running local tests to verify GitHub Actions workflow..."
 echo "1. Running tests from SecuriBench..."
 sbt "testOnly br.unb.cic.securibench.deprecated.SecuribenchTestSuite"
 
-echo "2. Running tests from TaintBench..."
-sbt "testOnly br.unb.cic.android.AndroidTaintBenchSuiteExperiment1Test"
-sbt "testOnly br.unb.cic.android.AndroidTaintBenchSuiteExperiment2Test"
+# echo "2. Running tests from TaintBench..."
+# sbt "testOnly br.unb.cic.android.AndroidTaintBenchSuiteExperiment1Test"
+# sbt "testOnly br.unb.cic.android.AndroidTaintBenchSuiteExperiment2Test"
 
 echo "All tests completed!"
