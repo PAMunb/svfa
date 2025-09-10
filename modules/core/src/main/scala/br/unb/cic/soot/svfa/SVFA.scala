fa9a2d32669d9bafa32408e907dc4c5302545bb5
jose clavo tafur <jclavotafur@gmail.com>
2025-09-10 15:36:01 -0300

    add logic to create json with findings

--- modules/core/src/main/scala/br/unb/cic/soot/svfa/SVFA.scala
+++ modules/core/src/main/scala/br/unb/cic/soot/svfa/SVFA.scala
@@ -1,7 +1,11 @@
 package br.unb.cic.soot.svfa
 
+import br.unb.cic.soot.graph.{GraphNode, Statement, StatementNode}
 import soot._
 import br.unb.cic.soot.svfa.configuration.SootConfiguration
+import ujson._
+import java.io.{FileWriter, BufferedWriter}
+import scala.util.parsing.json.JSONObject
 
 /** Base class for all implementations of SVFA algorithms.
   */
@@ -36,5 +40,46 @@ abstract class SVFA extends SootConfiguration {
     BigDecimal((endTime - startTime) / 1000000)
       .setScale(2, BigDecimal.RoundingMode.HALF_UP)
       .toDouble
+  }
+
+  def findConflictingPaths() = {
+    svg.findConflictingPaths().foreach(conflict => {
+
+      val source = conflict.head
+      val sourceJson = generateJsonFormat(source)
+
+      // Convert the parent object to a JSON string
+      val jsonString = write(sourceJson, indent = 4)
+
+      createFile(jsonString, "source.json")
+    })
+  }
+
+  private def generateJsonFormat(node: GraphNode) = {
+
+    val stmt = node.value.asInstanceOf[Statement]
+    val method = stmt.sootMethod
+
+    Obj(
+      "statement" -> Str(""),
+      "methodName" -> Str(method.getDeclaration),
+      "className" -> Str(stmt.className),
+      "lineNo" -> Num(stmt.sootUnit.getJavaSourceStartLineNumber),
+      "targetName" -> Str(""),
+      "targetNo" -> Num(0),
+      "IRs" -> Obj(
+        "type" -> Str("Jimple"),
+        "IRstatement" -> Str(stmt.stmt)
+      )
+    )
+  }
+
+  private def createFile(json: String, fileName: String) = {
+    val file = new BufferedWriter(new FileWriter(fileName))
+    try {
+      file.write(json)
+    } finally {
+      file.close()
+    }
   }
 }
