c21d927d3e5ea6de42958d24d765054fc658d247
jose clavo tafur <jclavotafur@gmail.com>
2025-09-11 10:49:40 -0300

    wip: CompareFindings

---
+++ modules/core/src/main/scala/br/unb/cic/soot/scripts/CompareFindings.scala
@@ -0,0 +1,54 @@
+package br.unb.cic.soot.scripts
+
+import java.io.File
+import scala.io.Source
+import ujson._
+
+import scala.collection.mutable
+
+object CompareFindings extends App {
+
+  private def getSourceIR(jsonData: Value): String = {
+    getSourceOrSinkIR(jsonData, "source")
+  }
+
+  private def getSinkIR(jsonData: Value): String = {
+    getSourceOrSinkIR(jsonData, "sink")
+  }
+
+  private def getSourceOrSinkIR(jsonData: Value, sourceOrSink: String): String = {
+    jsonData(sourceOrSink)("IRs")(0)("IRstatement").toString()
+  }
+
+  if (args.isEmpty) {
+      println("No parameters provided.")
+      System.exit(1)
+  }
+
+  val actualPathFindings = args(0)
+  // val expectedPathFindings = args(1)
+  // val resultPathFindings = args(2)
+
+  val actualFindings = new File(actualPathFindings)
+  // val expectedFindings = new File(expectedPathFindings)
+
+  val actualJsonFiles = actualFindings.listFiles().filter(_.getName.endsWith("_findings.json"))
+  val actualConflicts = new mutable.HashSet[(String, Set[(String, String)])]()
+
+  actualJsonFiles.map(file => {
+      val jsonString = Source.fromFile(file).getLines().mkString("\n")
+
+      // Parse the JSON string into a ujson.Value
+      val jsonData: Value = read(jsonString)
+
+      val fileName = jsonData("fileName").str
+      // Accessing findings
+      val conflicts = jsonData("findings").arr.map(finding => {
+        (getSourceIR(finding), getSinkIR(finding))
+      }).toSet
+
+      actualConflicts.add((fileName, conflicts))
+  })
+
+  actualConflicts.foreach(println)
+}
