0b3ba6ee58b2c742d139c4feeff28f39df80da20
jose clavo tafur <jclavotafur@gmail.com>
2025-12-01 22:04:02 -0300

    Mock  getCookies() method

--- modules/securibench/src/test/java/javax/http/mock/HttpServletRequest.java
+++ modules/securibench/src/test/java/javax/http/mock/HttpServletRequest.java
@@ -1,7 +1,13 @@
 package javax.servlet.http.mock;
 
+import javax.servlet.http.Cookie;
+
 public class HttpServletRequest {
     public String getParameter(String s) {
         return "secret";
+    }
+
+    public Cookie[] getCookies() {
+        return new Cookie[0];
     }
 }
